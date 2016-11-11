ed63b486ee6feb6bb56c07a1a09e36bce86c7333
Alias M6 <aliasmontoya719@gmail.com>
2016-11-11 12:55:20 -0700

    creation of post route

--- git-checked.go
+++ git-checked.go
@@ -11,9 +11,10 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 	"gopkg.in/gorp.v1"
-	"strconv"
+	// "strconv"
 )
 
+//TestCenter structure
 type TestCenter struct {
 	Id               int64
 	Center_name      string
@@ -25,6 +26,7 @@ type TestCenter struct {
 	Need_appointment bool
 }
 
+//initialization of connection to database
 var dbmap = initDb()
 
 func initDb() *gorp.DbMap {
@@ -35,24 +37,27 @@ func initDb() *gorp.DbMap {
 	return dbmap
 }
 
+//check error function
 func checkErr(err error, msg string) {
 	if err != nil {
 		log.Fatalln(msg, err)
 	}
 }
 
+// main function where routes live
 func main() {
 	r := gin.Default()
 	v1 := r.Group("api/v1")
 	{
 		v1.GET("/centers", GetCenters)
 		v1.GET("/centers/:id", GetCenter)
-		v1.POST("/centers", PostCenter)
+		// v1.POST("/centers", PostCenter)
 	}
 
 	r.Run(":8000")
 }
 
+//helper functions for routes
 func GetCenters(c *gin.Context) {
 	var centers []TestCenter
 	_, err := dbmap.Select(&centers, "SELECT * FROM testing_centers")
@@ -60,7 +65,7 @@ func GetCenters(c *gin.Context) {
 	if err == nil {
 		c.JSON(200, centers)
 	} else {
-		c.JSON(404, centers)
+		c.JSON(404)
 		fmt.Println(err)
 	}
 
@@ -68,30 +73,34 @@ func GetCenters(c *gin.Context) {
 }
 
 func GetCenter(c *gin.Context) {
+
 	id := c.Params.ByName("id")
 	var center TestCenter
 	err := dbmap.SelectOne(&center, "SELECT * FROM testing_centers WHERE id=$1", id)
-
 	fmt.Println(id)
-	fmt.Println(center)
-
 	if err == nil {
-		center_id, _ := strconv.ParseInt(id, 0, 64)
-
-		content := &TestCenter{
-			Id:               center_id,
-			Center_name:      center.Center_name,
-			Address:          center.Address,
-			Days_open:        center.Days_open,
-			Time_open:        center.Time_open,
-			Time_closed:      center.Time_closed,
-			Website:          center.Website,
-			Need_appointment: center.Need_appointment,
-		}
-		c.JSON(200, content)
+		c.JSON(200, center)
 	} else {
 		c.JSON(404, gin.H{"error": "center not found"})
 	}
 
-	// curl -i http://localhost:8080/api/v1/user/1
+}
+
+func PostUser(c *gin.Context) {
+	var center TestCenter
+	c.Bind(&center)
+	if center.Name != "" && center.Address != "" && center.Days_open != nil && center.Time_open != nil && center.Website != "" && center.Need_appointment != nil  {
+		insert, _ := dbmap.Exec(`INSERT INTO trial (Id, Center_name, Address, Days_open, Time_open, Time_closed, Website, Need_appointment)
+    VALUES ($1, $2)`, center.Name, center.Email)
+
+		if insert != nil {
+			c.JSON(201, gin.H{"success": "Added User"})
+		} else {
+			c.JSON(501, gin.H{"failed": "Insert failed"})
+		}
+	} else {
+		c.JSON(422, gin.H{"error": "fields are empty"})
+	}
+	// curl -i -X POST -H "Content-Type: application/json" -d "{ \"name\": \"Beau\", \"email\": \"b@o.com\" }"
+	// http://localhost:8080/api/v1/users
 }
